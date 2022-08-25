f7e574afc772c790af1caa69590f4028f5e727df
Kavtaradze <skavtaradze@centigen.co.uk>
2022-08-25 20:18:28 +0400

    Add router file

--- src/router/router.js
+++ src/router/router.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import DefaultLayout from "../core/components/layout/DefaultLayout";
+import Home from "@/modules/home/Home";
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes: [
-		{
-			path: '/',
-			name: 'dashboard',
-			component: DefaultLayout,
-			meta: {requiresAuth: true},
-			children: []
-		}
-	]
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'dashboard',
+      component: DefaultLayout,
+      meta: {requiresAuth: true},
+    }
+  ]
 })
 
 export default router;
